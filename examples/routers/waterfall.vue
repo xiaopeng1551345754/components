61e17cddda96c70e13aba27f3f4c3f10d485460a
chenmengqi <chenmengqi@autohome.com.cn>
2020-05-19 21:38:15 +0800

    新增瀑布流

--- examples/routers/waterfall.vue
+++ examples/routers/waterfall.vue
@@ -1,92 +1,109 @@
 <template>
-  <waterfall :loading-img="loadingImg" :list="list" @scroll-bottom="scrollBottom"></waterfall>
+  <waterfall
+    :loading-img="loadingImg"
+    :list="list"
+    @scroll-bottom="scrollBottom"
+    @select-item="selectItem"
+  ></waterfall>
 </template>
 <script>
-  export default  {
-    data() {
-      return {
-        loadingImg: '', // 懒加载图片
-        list: [
-          {
-            type: 'live',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            status: 'doing', //  进行中
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
-          {
-            type: 'static',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
+export default {
+  data() {
+    return {
+      loadingImg:
+        "http://n.sinaimg.cn/translate/34/w534h300/20180506/EZhs-hacuuvu0675818.jpg", // 懒加载图片
+      list: [
+        {
+          type: "live",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          status: "doing", //  进行中
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
+        {
+          type: "static",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
 
-          {
-            type: 'static',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
+        {
+          type: "static",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
 
-          {
-            type: 'static',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
+        {
+          type: "static",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
 
-          {
-            type: 'static',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
+        {
+          type: "static",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
 
-          {
-            type: 'static',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
-          {
-            type: 'live',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            status: 'done', //  结束
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
-          {
-            type: 'live',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            status: 'nostart', //  未开始
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
-          {
-            type: 'live',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            status: 'doing', //  进行中
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          },
-          {
-            type: 'live',
-            img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f',
-            status: 'doing', //  进行中
-            title: '八个字以内的 title',
-            exhibition_name: '展商名字展商名字'
-          }
-        ]
-      }
+        {
+          type: "static",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
+        {
+          type: "live",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          status: "done", //  结束
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
+        {
+          type: "live",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          status: "nostart", //  未开始
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
+        {
+          type: "live",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          status: "doing", //  进行中
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        },
+        {
+          type: "live",
+          src:
+            "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590483334&t=a427c29284cb086a3ec0b0b7609e976f",
+          status: "doing", //  进行中
+          title: "八个字以内的 title",
+          exhibition_name: "展商名字展商名字"
+        }
+      ]
+    };
+  },
+  methods: {
+    //  暴露事件方法
+    selectNode(node) {
+      console.log(node);
     },
-    methods: {
-      //  暴露事件方法
-      selectNode(node) {
-        console.log(node)
-      },
-      scrollBottom() {
-        console.log('scroll bottom')
-      }
-    }
+    scrollBottom() {
+      console.log("scroll bottom");
+    },
+    selectItem() {}
   }
+};
 </script>
--- src/components/waterfall/index.vue
+++ src/components/waterfall/index.vue
@@ -1,36 +1,160 @@
-<style lang="less">
-  
-</style>
 <template>
   <div class="hex-waterfall">
-    waterfall
+    <vue-waterfall-easy
+      :imgs-arr="list"
+      @scrollReachBottom="scrollBottom()"
+      @click="selectItem"
+      :max-cols="4"
+    >
+    <!-- <div class="img-info" slot-scope="props">
+      <p>第{{props.src+1}}张图片</p>
+    </div> -->
+    </vue-waterfall-easy>
   </div>
 </template>
 <script>
-  // import { throttle } from '../../utils/dom'
-  import clickOutSide from '../../directives/clickoutside.js'
-  export default {
-    name: 'waterfall',
-    directives: {
-      clickOutSide
-    },
-    props: {
-    },
-    data() {
-      return {
+// import { throttle } from '../../utils/dom'
+import clickOutSide from "../../directives/clickoutside.js";
+import vueWaterfallEasy from './vue-waterfall-easy/vue-waterfall-easy.vue'
+export default {
+  name: "waterfall",
+  components: {
+    vueWaterfallEasy
+  },
+  directives: {
+    clickOutSide
+  },
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return [];
       }
     },
-    watch: {
+    loadingImg: {
+      type: "",
+      default: ""
+    }
+  },
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  ready() {
+  },
+  methods: {
+    // init waterfall instance
+    init() {
+
     },
-    computed: {
+    scrollBottom() {
+      console.log('scrollBottom')
+      this.$emit("scrollBottom");
     },
-    ready() {
-    },
-    methods: {
-      // init waterfall instance
-      init () {
-        
-      }
+    selectItem() {
+      this.$emit("selectItem");
     }
   }
+};
 </script>
+<style lang="less">
+.hex-waterfall{
+  height: 500px;
+}
+.px-container {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.clearfix:before,
+.clearfix:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.clearfix {
+  zoom: 1;
+}
+.left {
+  float: left;
+}
+.font22 {
+  font-size: 22px;
+}
+.color-fff {
+  color: #fff;
+}
+.fh {
+  overflow: hidden;
+}
+[v-cloak] {
+  display: none !important;
+}
+.waterfall {
+  margin-top: 20px;
+}
+.px-waterfall {
+  width: calc(380px);
+  &:nth-child(3n + 2) {
+    margin: 0 30px;
+  }
+  img.bg-img {
+    border-radius: 8px;
+  }
+  h3 {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    height: 30px;
+    font-family: PingFangSC-Semibold;
+    font-weight: 600;
+    line-height: 30px;
+    letter-spacing: 6px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+  }
+  li {
+    background: #fff;
+    border-radius: 8px;
+    margin-bottom: 20px;
+  }
+}
+
+.px-hot-tag {
+  position: absolute;
+  top: 0;
+  border-radius: 8px;
+  width: 71px;
+  height: 30px;
+  img {
+    border-radius: 8px 0 8px 0;
+  }
+}
+.photo {
+  position: relative;
+  height: 25px;
+  .keywordbox {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    height: 25px;
+  }
+}
+.keyword {
+  height: 25px;
+  line-height: 25px;
+  padding: 0 22px;
+  text-align: center;
+  .color-666-line {
+    border-bottom: 2px solid #666666;
+  }
+  .red-line {
+    border-bottom: 2px solid #f65050;
+  }
+}
+</style>
