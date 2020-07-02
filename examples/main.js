6e9c03bb2b9c861270429e816d287c8bdbc0c0e7
wangyanjing <wangyanjing@wudaima.com>
2020-07-02 15:39:32 +0800

    首页视觉差效果

--- examples/main.js
+++ examples/main.js
@@ -1,91 +1,3 @@
-<<<<<<< HEAD
-/**
- * Created by aresn on 16/6/20.
- */
-import Vue from 'vue'
-import VueRouter from 'vue-router';
-import App from './app.vue';
-import stage from '../src/index';
-
-Vue.use(VueRouter);
-Vue.use(stage);
-// 开启debug模式
-Vue.config.debug = true;
-
-// 路由配置
-var router = new VueRouter();
-
-router.map({
-  '/waterfall': {
-    component: function(resolve) {
-      require(['./routers/waterfall.vue'], resolve);
-    }
-  },
-  '/navcate': {
-    component: function(resolve) {
-      require(['./routers/navcate.vue'], resolve);
-    }
-  },
-  '/area': {
-    component: function(resolve) {
-      require(['./routers/area.vue'], resolve);
-    }
-  },
-  '/timeline': {
-    component: function(resolve) {
-      require(['./routers/timeline.vue'], resolve);
-    }
-  },
-  '/datepicker': {
-    component: function(resolve) {
-      require(['./routers/datepicker.vue'], resolve);
-    }
-  },
-  '/hxkdate': {
-    component: function(resolve) {
-      require(['./routers/hxk_date.vue'], resolve);
-    }
-  },
-  '/timelinesingle': {
-    component: function(resolve) {
-      require(['./routers/timelinesingle.vue'], resolve);
-    }
-  },
-  '/board': {
-    component: function(resolve) {
-      require(['./routers/board.vue'], resolve);
-    }
-  },
-  'accordion': {
-    component: function(resolve) {
-      require(['./routers/accordion.vue'], resolve);
-    }
-  },
-  'cardmenu': {
-    component: function(resolve) {
-      require(['./routers/cardmenu.vue'], resolve);
-    }
-  },
-  'lanhemenu': {
-    component: function(resolve) {
-      require(['./routers/lanhemenu.vue'], resolve);
-    }
-  }
-});
-
-router.beforeEach(function() {
-  window.scrollTo(0, 0);
-});
-
-router.afterEach(function(transition) {
-
-});
-
-router.redirect({
-  '*': "/lanhemenu"
-});
-router.start(App, '#app');
-=======
 /**
  * Created by aresn on 16/6/20.
  */
@@ -133,9 +45,29 @@ router.map({
             require(['./routers/hxk_date.vue'], resolve);
         }
     },
-    '/mybutton': {
+    '/timelinesingle': {
         component: function(resolve) {
-            require(['./routers/mybutton.vue'], resolve);
+            require(['./routers/timelinesingle.vue'], resolve);
+        }
+    },
+    '/board': {
+        component: function(resolve) {
+            require(['./routers/board.vue'], resolve);
+        }
+    },
+    'accordion': {
+        component: function(resolve) {
+            require(['./routers/accordion.vue'], resolve);
+        }
+    },
+    'cardmenu': {
+        component: function(resolve) {
+            require(['./routers/cardmenu.vue'], resolve);
+        }
+    },
+    'lanhemenu': {
+        component: function(resolve) {
+            require(['./routers/lanhemenu.vue'], resolve);
         }
     },
     '/imgscroll': {
@@ -154,7 +86,6 @@ router.afterEach(function(transition) {
 });
 
 router.redirect({
-    '*': "/hxkdate"
+    '*': "/lanhemenu"
 });
 router.start(App, '#app');
->>>>>>> 视觉差动画
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import navCate from "./components/navcate/index.vue";
 import waterfall from "./components/waterfall/index.vue";
 import area from "./components/area/index.vue";
@@ -10,42 +9,8 @@ import board from './components/board/index.vue'
 import accordion from './components/accordion/index.vue'
 import cardmenu from './components/cardmenu/index.vue'
 import lanhemenu from './components/lanhemenu/index.vue'
-
-const Components = {
-  navCate,
-  waterfall,
-  area,
-  timeline,
-  datepicker,
-  hxkdate,
-  timelinesingle,
-  board,
-  accordion,
-  cardmenu,
-  lanhemenu
-};
-
-const install = function (Vue, opts = {}) {
-  Object.keys(Components).forEach((key) => {
-    Vue.component(Components[key].name || key, Components[key]);
-  });
-};
-
-// auto install
-if (typeof window !== "undefined" && window.Vue) {
-  install(window.Vue);
-}
-
-module.exports = Object.assign(Components, { install }); // eslint-disable-line no-undef
-=======
-import navCate from "./components/navcate/index.vue";
-import waterfall from "./components/waterfall/index.vue";
-import area from "./components/area/index.vue";
-import timeline from "./components/timeline/index.vue";
-import datepicker from "./components/datepicker/index.vue";
-import hxkdate from './components/hxk_date/index.vue'
-import mybutton from './components/mybutton/index.vue'
 import imgscroll from './components/imgscroll/index.vue'
+
 const Components = {
     navCate,
     waterfall,
@@ -53,7 +18,11 @@ const Components = {
     timeline,
     datepicker,
     hxkdate,
-    mybutton,
+    timelinesingle,
+    board,
+    accordion,
+    cardmenu,
+    lanhemenu,
     imgscroll
 };
 
@@ -69,4 +38,3 @@ if (typeof window !== "undefined" && window.Vue) {
 }
 
 module.exports = Object.assign(Components, { install }); // eslint-disable-line no-undef
->>>>>>> 视觉差动画
