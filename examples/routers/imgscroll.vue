65d26daae59886f2c1b5fdd6b45ee76e0cf538d2
yzf <1383658895@qq.com>
2020-07-09 11:41:16 +0800

    修改imgscroll效果

--- examples/routers/imgscroll.vue
+++ examples/routers/imgscroll.vue
@@ -1,43 +1,57 @@
 <template>
-  <div class="content">
-    <div class="wrap">
-      <imgscroll :list='list'  :default-rem='defaultRem' :change-rem='changeRem' :start-change='startChange' @select='select'></imgscroll>
+  <div class="screen">
+    <div class="content">
+      <div class="wrap">
+        <imgscroll
+          :list="list"
+          :default-rem="defaultRem"
+          :change-rem="changeRem"
+          :start-change="startChange"
+          @select="select"
+        ></imgscroll>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
-            list: [],
-            data: {
-              boxArr: [], // 各个盒子的位置高度信息
-            },
-            defaultRem: 0, // 初始的rem
-              changeRem: 2, // 要变化的rem距离
-              startChange: 0.5 // 并不是盒子刚刚触碰顶部，就开始修改背景图位置，而是到了盒子某个位置时，这个是
-          }
+      list: [],
+      data: {
+        boxArr: [], // 各个盒子的位置高度信息
+      },
+      defaultRem: 0, // 初始的rem
+      changeRem: 5, // 要变化的rem距离
+      startChange: 0.5, // 并不是盒子刚刚触碰顶部，就开始修改背景图位置，而是到了盒子某个位置时，这个是
+    };
   },
   methods: {
-     select (img) {
-      console.log(img)
+    select(img) {
+      console.log(img);
     },
-      
   },
-  ready () {
-    setTimeout(()=>{
-      for (let i=10;i--;) {
+  ready() {
+    setTimeout(() => {
+      for (let i = 10; i--; ) {
         this.list.push({
-          img: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg'
-        })
+          img:
+            "https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg",
+        });
       }
-    })
+    });
   },
- watch: {}
-}
+  watch: {},
+};
 </script>
 <style lang="less" scoped>
-.wrap {
+.screen {
+  overflow: auto;
   height: 100vh;
+  .content {
+    height: auto;
+    background: green;
+    padding-top: 50vh;
+  }
 }
 </style>
--- src/components/imgscroll/index.vue
+++ src/components/imgscroll/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="imgscroll-wrap" v-el:wrap>
-    <div 
-      @click='selectThis(item)'
+    <div
+      @click="selectThis(item)"
       v-for="(idx, item) in list"
       :key="idx"
-      :style="{backgroundImage: 'url('+item.img+')',height: height+ 'px'}"></div>
+      :style="{
+        backgroundImage: 'url(' + item.img + ')',
+        height: height + 'px',
+      }"
+    ></div>
   </div>
 </template>
 <script>
@@ -12,135 +16,128 @@ export default {
   name: "imgscroll",
   props: {
     list: {
-      type: Array
+      type: Array,
     },
     defaultRem: {
       type: Number,
-      default: 0
+      default: 0,
     },
     changeRem: {
       type: Number,
-      default: 2
+      default: 2,
     },
     startChange: {
       type: Number,
-      default: 0.5
+      default: 0.5,
     },
     height: {
       type: Number,
-      default: 400
-    }
+      default: 400,
+    },
   },
-  data () {
+  data() {
     return {
       boxArr: [],
-      dom: ''
+      dom: "",
     };
   },
-  ready () {
-    this.$nextTick(()=>{
-      this.dom = this.$els.wrap
-    })
+  ready() {
+    this.$nextTick(() => {
+      this.dom = document.querySelector(".screen");
+    });
   },
   methods: {
-    selectThis (img) {
-      this.$emit('select', img)
+    selectThis(img) {
+      this.$emit("select", img);
     },
-    init () {
-      this.$nextTick(()=>{
-        var uiWidth = 750
-        var docEl = this.dom
+    init() {
+      this.$nextTick(() => {
+        var uiWidth = 750;
+        var docEl = this.dom;
         var recalc = function () {
           // 乘以一个比例，作为rem的值，实现rem的值随着设别宽度的不同而不同
-          var clientWidth = docEl.clientWidth || 0
-          docEl.style.fontSize = 100 * (clientWidth / uiWidth) + 'px'
-        }
-        recalc() // 先自动触发一下
-        window.addEventListener('resize', recalc, false) // 添加页面监听事件
-  
-        this.getBoxArr() // 获取各个盒子的位置高度信息
+          var clientWidth = docEl.clientWidth || 0;
+          docEl.style.fontSize = 100 * (clientWidth / uiWidth) + "px";
+        };
+        recalc(); // 先自动触发一下
+        window.addEventListener("resize", recalc, false); // 添加页面监听事件
+
+        this.getBoxArr(); // 获取各个盒子的位置高度信息
         if (this.boxArr && this.boxArr.length) {
-          this.initPosition() // 初始化功能
+          this.initPosition(); // 初始化功能
         }
-      })
+      });
     },
-    getBoxArr () {
-      var doms = document.querySelectorAll('.imgscroll-wrap > div')
-      this.boxArr = []
+    getBoxArr() {
+      var doms = document.querySelectorAll(".imgscroll-wrap > div");
+      this.boxArr = [];
       if (doms && doms.length) {
         for (var i = 0; i < doms.length; i++) {
-          var boxDom = doms[i]
+          var boxDom = doms[i];
           this.boxArr.push({
             boxDom: boxDom, // 盒子的dom
             startSpace: boxDom.offsetTop + (1 + this.startChange), // 开始修改背景的位置（盒子的顶部，距离页面文档顶部的距离）
             boxHeight: boxDom.offsetHeight, // 盒子高度
             endSpace:
-              boxDom.offsetTop +
-              (1 + this.startChange) +
-              boxDom.offsetHeight // 结束修改背景的位置（盒子的底部，距离页面文档顶部的距离）
-          })
+              boxDom.offsetTop + (1 + this.startChange) + boxDom.offsetHeight, // 结束修改背景的位置（盒子的底部，距离页面文档顶部的距离）
+          });
         }
       }
     },
-    initPosition () {
-      this.setPosition()
-      this.dom.addEventListener('scroll', this.windowScroll)
+    initPosition() {
+      this.setPosition();
+      this.dom.addEventListener("scroll", this.windowScroll);
     },
-    windowScroll () {
-      this.setPosition()
+    windowScroll() {
+      this.setPosition();
     },
-    setPosition () {
-      const boxArr = this.boxArr
-      if (!boxArr || !boxArr.length) return
-      var scrollTop = this.dom.scrollTop
+    setPosition() {
+      const boxArr = this.boxArr;
+      if (!boxArr || !boxArr.length) return;
+      var scrollTop = this.dom.scrollTop;
       for (var i = 0; i < boxArr.length; i++) {
         var boxDom = boxArr[i].boxDom,
           startSpace = boxArr[i].startSpace,
           endSpace = boxArr[i].endSpace,
-          boxHeight = boxArr[i].boxHeight
+          boxHeight = boxArr[i].boxHeight;
         if (scrollTop < startSpace) {
           // 还没到的dom
           boxDom.style.backgroundPosition =
-            ' center ' + this.defaultRem + 'rem'
+            " center " + this.defaultRem + "rem";
         } else if (scrollTop > endSpace) {
           // 已经过去了的dom
           boxDom.style.backgroundPosition =
-            ' center ' + this.defaultRem + this.changeRem + 'rem'
+            " center " + this.defaultRem + this.changeRem + "rem";
         } else {
           // 正好处于，开始设置
           var remNum =
             this.defaultRem +
-            ((scrollTop - startSpace) / boxHeight) * this.changeRem // 按比例计算
-          let string = ' center ' + remNum + 'rem'
-          boxDom.style['backgroundPosition'] = string
+            ((scrollTop - startSpace) / boxHeight) * this.changeRem; // 按比例计算
+          let string = " center " + remNum + "rem";
+          boxDom.style["backgroundPosition"] = string;
         }
       }
-    }
+    },
   },
   watch: {
     list: {
       immediate: true,
-      handler (n) {
+      handler(n) {
         this.$nextTick(() => {
-          this.init()
-        })
-      }
-    }
-  }
+          this.init();
+        });
+      },
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
 .imgscroll-wrap {
-  height: 100%;
+  height: max-content;
   overflow: auto;
 }
 .imgscroll-wrap div {
   background-repeat: no-repeat;
   background-size: 100% 100%;
 }
-//隐藏滚动条
-// ::-webkit-scrollbar {
-//           /*滚动条整体样式*/
-//           width : 0px
-//           }
 </style>
