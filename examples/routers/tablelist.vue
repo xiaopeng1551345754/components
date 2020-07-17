43237663a2ae3f0d37214b919075c7e07efd5b13
liuxinxin <proven_liu@163.com>
2020-07-17 10:18:36 +0800

    升级新奥表格

--- examples/routers/tablelist.vue
+++ examples/routers/tablelist.vue
@@ -1,6 +1,5 @@
 <template>
   <tablelist :table-list="tableList"
-             style="width: 1088px;"
              @changestatus="changestatus"
              @clickbtn="clickbtn"></tablelist>
 </template>
@@ -11,343 +10,127 @@ export default {
       tableList: [
         [
           {
-            id: 1,
-            name: '项目名称',
+            name: '编号',
+            label: 'id',
+            width: '50',
+            textAlign: 'center'
+          },
+          {
+            name: '姓名',
             label: 'name',
-            width: '340'
-          }, {
-            id: 2,
-            name: '项目负责人',
-            label: 'leader',
-            width: '102'
-          }, {
-            id: 3,
-            name: '联系方式',
-            label: 'phone',
-            width: '122'
-          }, {
-            id: 4,
-            name: '处理状态',
-            label: 'status',
-            width: '108'
-          }, {
-            id: 5,
-            name: '公开',
-            label: 'isPublic',
-            width: '92'
-          }, {
-            id: 6,
+            width: '150',
+            textAlign: 'center'
+          },
+          {
+            name: '电话号码',
+            label: 'number',
+            width: '150',
+            textAlign: 'center'
+          },
+          {
+            name: '家庭住址',
+            label: 'address',
+            width: '250',
+            textAlign: 'left'
+          },
+          {
+            name: '公开信息',
+            label: 'public',
+            type: 'switch',
+            width: '100',
+            textAlign: 'center'
+          },
+          {
             name: '操作',
-            label: 'handle',
-            width: '324'
+            label: 'button',
+            width: '250',
+            textAlign: 'center'
           }
         ],
         [
           {
-            name: "结束就看你曾经看快",
-            leader: '李华',
-            phone: '13622333333',
-            status: 1, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: false, // 1: 是  0: 否
-            handle: [
+            id: 1,
+            name: '小明',
+            number: '12432436547',
+            address: '内存卡就是对你曾经看',
+            button: [
               {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#BFBFBF',
-                width: '60'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '已通知专家',
-                background: '#FFFFFF',
-                color: '#3480FF',
-                width: '88'
+                name: '编辑',
+                background: '#598ABE',
+                color: '#fff',
+                bdcolor: '#598ABE',
+                width: '56'
               }
-            ]
+            ],
+            public: true
           },
           {
-            name: "结束就看你曾经看快九十年代就开始你",
-            leader: '李华',
-            phone: '13622333333',
-            status: 2, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: false, // 1: 是  0: 否
-            handle: [
+            id: 1,
+            name: '小明',
+            number: '12432436547',
+            address: '内存卡就是对你曾经看你打开就是你吃亏的就是你曾经就开始吃亏的是你曾经看',
+            button: [
               {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60',
-                disabled: 'disabled'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
+                name: '编辑',
+                background: '#598ABE',
+                color: '#fff',
+                bdcolor: '#598ABE',
+                width: '56'
               }
-            ]
+            ],
+            public: true,
+            hsjdhf: 'nfkdnvk'
           },
           {
-            name: "结束就看你曾经看快九十年代就开始你就就是你曾经蝴蝶结考试成绩的考生那就是你曾经的",
-            leader: '李华',
-            phone: '13622333333',
-            status: 3, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: true, // 1: 是  0: 否
-            handle: [
+            id: 1,
+            name: '小明',
+            number: '12432436547',
+            // address: '内存卡就是对你曾经看',
+            button: [
               {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60',
-                disabled: 'disabled'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
+                name: '编辑',
+                background: '#598ABE',
+                color: '#fff',
+                bdcolor: '#598ABE',
+                width: '56'
               }
-            ]
+            ],
+            public: false,
+            hsjdhf: 'nfkdnvk'
           },
           {
-            name: "结束就看你曾经看快九十年代就开始你就就是你曾经保持绝对是白菜价",
-            leader: '李华',
-            phone: '13622333333',
-            status: 1, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: false, // 1: 是  0: 否
-            handle: [
+            id: 1,
+            name: '小明',
+            number: '12432436547',
+            address: '内存卡就是对你曾经看',
+            button: [
               {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
+                name: '编辑',
+                background: '#598ABE',
+                color: '#fff',
+                bdcolor: '#598ABE',
+                width: '56'
               }
-            ]
+            ],
+            public: true,
+            hsjdhf: 'nfkdnvk'
           },
           {
-            name: "结束就看你曾经看快九十年代就开始",
-            leader: '李华',
-            phone: '13622333333',
-            status: 3, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: true, // 1: 是  0: 否
-            handle: [
+            id: 1,
+            name: '小明',
+            number: '12432436547',
+            address: '内存卡就是对你曾经看',
+            button: [
               {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60',
-                disabled: 'disabled'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
+                name: '编辑',
+                background: '#598ABE',
+                color: '#fff',
+                bdcolor: '#598ABE',
+                width: '56'
               }
-            ]
-          },
-          {
-            name: "结束就看你曾经看快九十年代就开始你就就是你曾经不定时举办超级多",
-            leader: '李华',
-            phone: '13622333333',
-            status: 1, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: false, // 1: 是  0: 否
-            handle: [
-              {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
-              }
-            ]
-          },
-          {
-            name: "结束就看你曾经看快九",
-            leader: '李华',
-            phone: '13622333333',
-            status: 2, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: false, // 1: 是  0: 否
-            handle: [
-              {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60',
-                disabled: 'disabled'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
-              }
-            ]
-          },
-          {
-            name: "结束就看你曾经看快九十年代",
-            leader: '李华',
-            phone: '13622333333',
-            status: 3, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: true, // 1: 是  0: 否
-            handle: [
-              {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60',
-                disabled: 'disabled'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
-              }
-            ]
-          },
-          {
-            name: "结束就看你曾经看快九十年代就开始你胆结石不吃醋大家能见到你曾经",
-            leader: '李华',
-            phone: '13622333333',
-            status: 1, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: false, // 1: 是  0: 否
-            handle: [
-              {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
-              }
-            ]
-          },
-          {
-            name: "结束就看你曾经看快九十年代就开始你就就是你曾经",
-            leader: '李华',
-            phone: '13622333333',
-            status: 3, // 1: 组队中 2: 驳回 3: 通过,
-            isPublic: true, // 1: 是  0: 否
-            handle: [
-              {
-                name: '查看',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '处理',
-                background: '#fff',
-                color: '#595959',
-                width: '60',
-                disabled: 'disabled'
-              }, {
-                name: '删除',
-                background: '#fff',
-                color: '#595959',
-                width: '60'
-              }, {
-                name: '通知专家',
-                background: '#3480FF',
-                color: '#FFFFFF',
-                width: '88'
-              }
-            ]
+            ],
+            public: false,
+            hsjdhf: 'nfkdnvk'
           }
         ]
       ]
@@ -355,10 +138,10 @@ export default {
   },
   methods: {
     changestatus (data) {
-      // console.log(data);
+      // console.log(data, '点击开关');
     },
     clickbtn (data, btn) {
-      // console.log(data, btn);
+      // console.log(data, btn, '点击按钮');
     }
   }
 
